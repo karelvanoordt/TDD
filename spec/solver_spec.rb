81b708720a9a78d8383c9de1e9afc7b2433ffad1
karelvanoordt <63868555+karelvanoordt@users.noreply.github.com>
2022-02-03 08:19:23 -0500

    Add error test cases to factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -12,6 +12,23 @@ describe 'test methods' do
         expect(f).to eq (24)
     end
 
+    it 'should return the factorial for the number n' do
+        f  = @solver.factorial(7)
+        expect(f).to eq (5040)
+    end
+
+    it 'should return an error for zero' do
+        f  = @solver.factorial(0)
+        expect(f).to eq (1)
+    end
+
+    it 'should return an error for the negative number n' do
+        f  = @solver.factorial(-2)
+        expect(f).to raise_error('The number should be greater than zero')
+    end
+
+
+
     ## reverse method validation
     it 'should return the reversed string' do
         rev = @solver.reverse('hello')
