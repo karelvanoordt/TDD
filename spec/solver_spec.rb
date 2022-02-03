f3f112c3ddeb857a54ff946ae28a6f3c6d6a9c77
karelvanoordt <63868555+karelvanoordt@users.noreply.github.com>
2022-02-03 08:06:12 -0500

    Add solver object to test file

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,33 +2,35 @@ require './solver'
 
 describe 'test methods' do
 
+    @solver = Solver.new
+
     ## factorial method validation
     it 'should return the factorial for the number n' do
-        f  = factorial(4)
+        f  = @solver.factorial(4)
         expect(f).to eq (24)
     end
 
     ## reverse method validation
     it 'should return the reversed string' do
-        rev = reverse('hello')
+        rev = @solver.reverse('hello')
         expect(rev).to eq ('olleh')
     end
 
     ## fizzbuzz method validation 1
     it 'for a number divisible by 3, should return fizz' do
-        fizz = fizzbuzz(9)
+        fizz = @solver.fizzbuzz(9)
         expect(fizz).to eq ('fizz')
     end
 
     ## fizzbuzz method validation 2
     it 'for a number divisible by 5, should return buzz' do
-        buzz = fizzbuzz(10)
+        buzz = @solver.fizzbuzz(10)
         expect(buzz).to eq ('buzz')
     end
 
     ## fizzbuzz method validation 3
     it 'for a number divisible by 3 and 5, should return fizzbuzz' do
-        fizzbuzz = fizzbuzz(15)
+        fizzbuzz = @solver.fizzbuzz(15)
         expect(fizzbuzz).to eq ('fizzbuzz')
     end
 
