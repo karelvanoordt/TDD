b626b96728aace6dceaed3872948ad6f5732202b
Shahier Nashaat <shahiern@yahoo.com>
2022-02-03 15:51:08 +0200

    Create solver class

--- solver.rb
+++ solver.rb
@@ -1,4 +1,20 @@
-# frozen_string_literal: true
-
 class Solver
+  def factorial(num)
+    raise 'The number should be greater than zero' if num.negative?
+    return 1 if num.zero?
+
+    num * factorial(num - 1)
+  end
+
+  def reverse(word)
+    word.chars.reverse.join
+  end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+
+    num.to_s
+  end
 end
